f8ea6820494b017c046d0af059d846150e784b09
Roland4444 <turbopollux@rambler.ru>
2019-02-07 17:26:29 +0400

    ftpworks fine; ftcClient test;

--- src/test/java/util/crypto/TestSign2001Test.java
+++ src/test/java/util/crypto/TestSign2001Test.java
@@ -6,6 +6,7 @@ import org.bouncycastle.operator.OperatorCreationException;
 import org.junit.Test;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -20,6 +21,7 @@ import static org.junit.Assert.*;
 
 public class TestSign2001Test {
     TestSign2001 ts = new TestSign2001();
+    Gost3411Hash hasher = new Gost3411Hash();
 
     public TestSign2001Test() throws NoSuchAlgorithmException, CertificateException, NoSuchProviderException, KeyStoreException, IOException {
     }
@@ -35,7 +37,44 @@ public class TestSign2001Test {
         assertNotEquals(null, ts.getCert());
     }
 
-    
+
+    @Test
+    public void generatesPhotoAudioCryptos() throws Exception {
+        String photofile =  "/home/roland/IdeaProjects/sm3/photos/p1.jpg";
+        String soundfile =  "/home/roland/IdeaProjects/sm3/etalon.wav";
+        File f1 = new File(photofile);
+        File f2 = new File(soundfile);
+        assertTrue(f1.exists());
+        assertTrue(f2.exists());
+        byte[] imageData = Files.readAllBytes(f1.toPath());
+        byte[] soundData = Files.readAllBytes(f2.toPath());
+
+        System.out.println("\nSOUND HASH=>\n"+hasher.h_Base64rfc2045(soundData));
+
+        System.out.println("\nPhoto HASH=>\n"+hasher.h_Base64rfc2045(imageData));
+
+        String PKSC7Sound = hasher.base64(ts.SMEV3PKSC7(soundData));
+        String PKSC7Photo = hasher.base64(ts.SMEV3PKSC7(imageData));
+
+        System.out.println("\nPKSC7 sound sig=>\n"+PKSC7Sound);
+
+        System.out.println("\nPKSC7 photo sig=>\n"+PKSC7Photo);
+    }
+
+    @Test
+    public void generatesPhotoAudioCryptos2() throws Exception {
+
+        String soundfile =  "/home/roland/IdeaProjects/sm3/result.wav";
+        File f2 = new File(soundfile);
+        assertTrue(f2.exists());
+        byte[] soundData = Files.readAllBytes(f2.toPath());
+        System.out.println("\nSOUND HASH=>\n"+hasher.h_Base64rfc2045(soundData));
+        String PKSC7Sound = hasher.base64(ts.SMEV3PKSC7(soundData));
+        System.out.println("\nPKSC7 sound sig=>\n"+PKSC7Sound);
+
+
+    }
+
 
 
 }
--- src/test/java/util/ftpClientTest.java
+++ src/test/java/util/ftpClientTest.java
@@ -210,6 +210,75 @@ public class ftpClientTest {
 
     }
 
+    @Test
+    public void uploadanddownloadPythonStyle() throws IOException {
+        String photofile = "/home/roland/IdeaProjects/sm3/photos/p1.jpg";
+        String soundfile = "/home/roland/IdeaProjects/sm3/tested.wav";
 
+        ftpClient ftpcl = new ftpClient(smev3addr, "anonymous", "smev");
+        ftpcl.port = 21;
+
+        print("port=>>"+ftpcl.port);
+        assertEquals(0, ftpcl.open());
+
+        String imageuuid = new timeBasedUUID().generate();
+        String sounduuid = new timeBasedUUID().generate();
+
+        assertEquals(0, ftpcl.mkdir(imageuuid));
+        assertEquals(0, ftpcl.mkdir(sounduuid));
+
+        String storesound = sounduuid + "/tested.wav";
+        String storeimage = imageuuid + "/tested.jpg";
+
+        assertEquals(0, ftpcl.uploadfile(soundfile, storesound));
+        assertEquals(0, ftpcl.uploadfile(photofile, storeimage));
+
+        print("\n\n\nEXECUTE >>>>>"+ storesound);
+        print("EXECUTE >>>>>"+ storeimage);
+
+        ftpcl.close();
+
+
+    }
+
+
+    public void print(Object input){
+        System.out.println(input);
+    }
+
+/*
+
+    from ftplib import FTP
+import uuid
+import os
+            imagename = '/home/roland/IdeaProjects/sm3/photos/p1.jpg'
+    soundname = '/home/roland/IdeaProjects/sm3/tested.wav'
+    imagefile=open(imagename,'rb')
+    soundfile=open(soundname,'rb')
+    print( os.path.getsize(imagename))
+    print( os.path.getsize(soundname))
+
+    session = FTP("smev3-n0.test.gosuslugi.ru")
+session.login()
+
+    imageuuid = str(uuid.uuid1())
+    sounduuid = str(uuid.uuid1())
+
+            session.mkd(imageuuid)
+            session.mkd(sounduuid)
+
+    commandstoresound = 'STOR '+sounduuid+'/tested.wav'
+    commandstoreimage = 'STOR '+imageuuid+'/tested.jpg'
+
+    print("EXECUTE >>>>>", commandstoresound)
+session.storbinary(commandstoresound, soundfile)
+
+    print("EXECUTE >>>>>", commandstoreimage)
+session.storbinary(commandstoreimage, imagefile)
+
+            session.quit()
+
+
+    */
 
 }
